862cb090a0f05aaef278f2afef58c909db945346
Alfredo Suárez Romero <48132178+sibokdev@users.noreply.github.com>
2024-02-26 22:47:53 -0600

    Added basic controller test

---
+++ src/test/java/com/code/challenge/web/MovieControllerMvcTests.java
@@ -0,0 +1,31 @@
+package com.code.challenge.web;
+
+import com.code.challenge.controllers.MovieController;
+import com.code.challenge.domain.MovieNotFoundException;
+import com.code.challenge.services.MovieService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@WebMvcTest(MovieController.class)
+class MovieControllerMvcTests {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private MovieService movieService;
+    @Test
+    void whenGetMovieNotExistingThenShouldReturn404() throws Exception {
+        String eidr = "73737313940";
+        given(movieService.viewMovieDetails(eidr))
+                .willThrow(MovieNotFoundException.class);
+        mockMvc
+                .perform(get("/movies/" + eidr))
+                .andExpect(status().isNotFound());
+    }
+
+}
+
